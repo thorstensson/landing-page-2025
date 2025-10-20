8691d7fd98769d096f8fc3e54107fb53e2f11304
Thomas <thomas.thorstensson@gmail.com>
2025-10-20 22:51:35 +0200

    thomasjt.com is no more. added netlify.toml file to get the redirect up to my main folio domain without upsetting google seo robots.

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,63 @@ definePageMeta({
 })
 
 useHead({
-  title: "Creative Developer • Thorstensson"
+  title: "Thomas Thorstensson - Creative Developer",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Creative Developer Thomas Thorstensson. New portfolio launching October 2025 at www.thomasthorstensson.com",
+    },
+    {
+      name: "keywords",
+      content:
+        "Thomas Thorstensson, Creative Developer, Web Developer, Portfolio, thomasthorstensson.com",
+    },
+    { name: "author", content: "Thomas Thorstensson" },
+    { name: "robots", content: "noindex, nofollow" },
+
+    // Open Graph / Facebook
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://www.thomasthorstensson.com/" },
+    {
+      property: "og:title",
+      content: "Thomas Thorstensson - Creative Developer",
+    },
+    {
+      property: "og:description",
+      content:
+        "Creative Developer Thomas Thorstensson. New portfolio launching October 2025.",
+    },
+    { property: "og:site_name", content: "Thomas Thorstensson" },
+
+    // Twitter
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:url", content: "https://www.thomasthorstensson.com/" },
+    {
+      name: "twitter:title",
+      content: "Thomas Thorstensson - Creative Developer",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "Creative Developer Thomas Thorstensson. New portfolio launching October 2025.",
+    },
+
+    // Additional SEO
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+  ],
+  link: [
+    {
+      rel: "alternate",
+      hreflang: "en",
+      href: "https://www.thomasthorstensson.com/",
+    },
+  ],
 })
 
 const scrollConfig = reactive({
-  text: "                        New Folio coming October 12th 2025 @ www.thomasthorstensson.com (nothing there yet). Thanks.",
+  text: "Redirecting to new portfolio...",
 })
 </script>
 
@@ -16,32 +68,13 @@ const scrollConfig = reactive({
   <div class="page-wrapper">
     <div class="launch">
       <h1 class="launch__heading">
-        New Folio: Oct 12 2025<br/>@ www.thomasthorstensson.com
+        New Folio<br />@ www.thomasthorstensson.com
       </h1>
-      <p class="launch__subtext">Meanwhile Visit</p>
-      <div class="launch__links">
-        <a
-          href="https://mp3playerp5.thomasthorstensson.com"
-          target="_blank"
-          class="launch__link"
-        >
-          mp3playerp5.thomasthorstensson.com
-        </a>
-        <a
-          href="https://palestine-awareness.thomasthorstensson.com"
-          target="_blank"
-          class="launch__link"
-        >
-          palestine-awareness.thomasthorstensson.com
-        </a>
-      </div>
     </div>
-    <UITextScroller :text="scrollConfig.text" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .page-wrapper {
   display: flex;
   justify-content: center;
@@ -69,7 +102,7 @@ const scrollConfig = reactive({
   &__subtext {
     font-family: $sans-text;
     color: $secondary;
-    font-size: 1.2rem;
+    font-size: 2.2rem;
     font-weight: $sans-text-wgt;
     margin-bottom: 1.5rem;
   }
@@ -90,7 +123,7 @@ const scrollConfig = reactive({
     transition: color 0.3s ease;
 
     &:hover {
-      color:$accent1;
+      color: $accent1;
     }
   }
 }
